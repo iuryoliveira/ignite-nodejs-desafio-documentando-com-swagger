8af2316a232a97e5204d67099f742b68c272d0a7
Iury Oliveira <iury@rarolabs.com.br>
2021-10-06 14:01:19 -0300

    first commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const emailAlreadyRegistered = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyRegistered) {
+      throw new Error("Email already registered.");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (user) {
+      this.usersRepository.turnAdmin(user);
+    } else {
+      throw new Error("Usuário não encontrado");
+    }
+
+    return user;
+  }
+}
+
+export { TurnUserAdminUseCase };
